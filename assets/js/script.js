0fd2ca809dff9aa3c081bff6b747ff69b9f03766
Ayden Krohn <krohnayden@gmail.com>
2023-10-30 21:26:35 -0500

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var breedSearchEl = document.querySelector('#breedSelector');
 var availablePetsEL = document.querySelector('#availablePets');
-var breedURL = `https://dog.ceo/api/breed/hound/images`
+var breedForm = document.querySelector('#breedForm');
 
 fetch(`https://dog.ceo/api/breeds/list/all`)
     .then(function (response) {
@@ -18,10 +18,21 @@ fetch(`https://dog.ceo/api/breeds/list/all`)
         }
     })
 
-fetch(breedURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+breedForm.addEventListener('submit', function (e) {
+    console.log("form submitted");
+    e.preventDefault();
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${breedSearchEl.value}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.items);
+            var dataBooks = data.items
+            for(i=0; i < dataBooks.length; i++ ) {
+                console.log(dataBooks[i]);
+            }    
+        })
+   
+
+
+})
